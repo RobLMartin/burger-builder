b40db17c6a838537c2910476a7a83665365e883e
Rob Martin <rmartin3@alumni.unca.edu>
2017-12-28 19:03:58 -0500

    add backdrop to modal with click to cancel

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -29,6 +29,10 @@ class BurgerBuilder extends Component {
         purchasing: false
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+
     purchaseHandler = () => {
         this.setState({purchasing: true});
     }
@@ -89,7 +93,7 @@ class BurgerBuilder extends Component {
 
         return(
             <Auxiliary>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
