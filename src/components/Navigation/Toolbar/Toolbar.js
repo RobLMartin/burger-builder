d307c174f56cb2766f9bdced848447154588ff4d
Rob Martin <rmartin3@alumni.unca.edu>
2017-12-29 16:16:30 -0500

    nav updates

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => {
     return (
@@ -8,7 +9,7 @@ const toolbar = (props) => {
             <Logo />
             <div>Menu</div>
             <nav>
-                ...
+                <NavigationItems />
             </nav>
         </header>
     );
